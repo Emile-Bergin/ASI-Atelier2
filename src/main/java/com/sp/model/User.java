0d2c766a7af9ca28d88870462eef515e946f34c3
clement <clement.dethoor@yahoo.fr>
2021-05-11 14:07:45 +0200

    push

--- src/main/java/com/sp/model/User.java
+++ src/main/java/com/sp/model/User.java
@@ -6,6 +6,7 @@ public class User {
 	private String name;
 	private String surname;
 	private String password;
+	
 	public User() {
 	}
 
--- src/main/java/com/sp/rest/AuthController.java
+++ src/main/java/com/sp/rest/AuthController.java
@@ -2,22 +2,25 @@ package com.sp.rest;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
 public class AuthController {
 	
 	@RequestMapping("/api/auth/signup")
-	public String sayHello() {
+	public String signup() {
+		
 		return "hello signup";
 	}
 	
-	@RequestMapping("/api/auth/login")
-	public String sayHello1() {
+	@RequestMapping(method = RequestMethod.POST, value = "/api/auth/login")
+	public String login() {
 		return "<h1>hello login</h1>";
 	}
 	
-	@RequestMapping("/api/auth/logout")
-	public String sayHello2() {
+	@RequestMapping(method = RequestMethod.POST , value = "/api/auth/logout")
+	public String logout() {
+		
 		return "hello login";
 	}
 
