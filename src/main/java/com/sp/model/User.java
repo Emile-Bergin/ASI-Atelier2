b963b2ed86d5a902225644ae9e9968e533cea2bb
Hugo Bonucci <hugo.bonucci@tpc21.cpe.fr>
2021-05-11 13:59:50 +0200

    Pour auth

--- src/main/java/com/sp/model/User.java
+++ src/main/java/com/sp/model/User.java
@@ -5,16 +5,15 @@ public class User {
 	private int id;
 	private String name;
 	private String surname;
-	private String password;
 	public User() {
 	}
 
-	public User(int id,String name, String surname, String password) {
+	public User(int id,String name, String surname) {
 		super();
 		this.id=id;
 		this.name = name;
 		this.surname = surname;
-		this.password = password;
+		
 		
 	}
 
