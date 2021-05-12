f00fc8446ac7b3ff0d0f0a3bfb1f7bbf20b5fd23
Hugo Bonucci <hugo.bonucci@tpc22.cpe.fr>
2021-05-12 10:47:11 +0200

    Modif JavaScript pour inscription user

--- src/main/resources/static/js/addUser.js
+++ src/main/resources/static/js/addUser.js
@@ -1,6 +1,6 @@
 $(document ).ready(function() {
     $("#submit").click(function () {
-        var data = {username: $("#username").val(), password: $("#password").val()};
+        var data = {username: $("#username").val(), password: $("#surname").val(), password: $("#password").val()};
         console.log(data);
         $.post({
             url: "api/auth/signup",
