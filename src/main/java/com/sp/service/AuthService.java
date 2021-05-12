7fd106a72b1091cd58171541d37bb8cdd11954bc
Grégoire Chateauneuf <gregoire.chateauneuf@cpe.fr>
2021-05-12 08:52:45 +0200

    authService v3

--- src/main/java/com/sp/service/AuthService.java
+++ src/main/java/com/sp/service/AuthService.java
@@ -1,39 +1,40 @@
 package com.sp.service;
-  import org.springframework.stereotype.Service;
-  import com.sp.service.UserService;
-  import com.sp.dto.LoginDO;  
+import org.springframework.stereotype.Service;
+import com.sp.service.UserService;
+//import com.sp.repository.UserRepository;
+import com.sp.dto.LoginDTO;  
   
 
-  @Service
-  public class AuthService {
-  int id;
+@Service
+public class AuthService {
+int id;
 
   	
-       public boolean signup(LoginDTO user) {
-     	 if(com.sp.service.UserService.SaveUser(user)==true){
-     	 	return true;
-     	 }
-     	 else{
-     	 	return false;
-     	 }	 
-      }
+   public boolean signup(LoginDTO user) {
+     if(com.sp.service.UserService.SaveUser(user)==true){
+     	return true;
+     }
+     else{
+    	 return false;
+     }	 
+   }
       
        /*
-      public boolean login(LoginDTO user) {
-    	 if((UserRepository.findByUsername(user.getUsername()) == true && (UserRepository.findByUsername(user.getUsername()).getPassword() == user.getPassword()))
-    		return true;
-    	 else
-    		new UsernameNotFoundException("user not found");
-    		return false;
-      }
-      
-      public boolean logout(loginDTO user) {
-    	  if((UserRepository.findByUsername(user.getUsername()).deleteToken(user))== true){
-    	  	return true;
-    	  }
-    	  else {
-    	  	return false;
-    	  }
-      }*/
-
+  public boolean login(LoginDTO user) {
+	 if((UserRepository.findByUsername(user.getUsername()) == true && (UserRepository.findByUsername(user.getUsername()).getPassword() == user.getPassword()))
+		return true;
+	 else
+		new UsernameNotFoundException("user not found");
+		return false;
   }
+  
+  public boolean logout(loginDTO user) {
+	  if((UserRepository.findByUsername(user.getUsername()).deleteToken(user))== true){
+	  	return true;
+	  }
+	  else {
+	  	return false;
+	  }
+  }*/
+
+}
