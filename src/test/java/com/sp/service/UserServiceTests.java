04d7c9aadb484a6203573fe09b2a2b0e4f229c39
emile bergin <berginemile098@gmail.com>
2021-05-25 15:01:23 +0200

    remove unsued import

--- src/test/java/com/sp/service/UserServiceTests.java
+++ src/test/java/com/sp/service/UserServiceTests.java
@@ -8,7 +8,6 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
 
