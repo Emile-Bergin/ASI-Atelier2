b38d99f4df329fbc31a1ee86cc682649e3164f38
clement <clement.dethoor@yahoo.fr>
2021-05-25 14:31:40 +0200

    cookie ok

--- src/main/java/com/sp/rest/AuthController.java
+++ src/main/java/com/sp/rest/AuthController.java
@@ -4,11 +4,14 @@ import com.sp.dto.LoginDTO;
 import com.sp.model.UserEntity;
 import com.sp.service.AuthService;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class AuthController {
@@ -26,14 +29,26 @@ public class AuthController {
 	
 	@RequestMapping(method = RequestMethod.POST, value = "/api/auth/login",
 			consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-	public UserEntity login( LoginDTO loginDTO) {
-		return authService.login(loginDTO);
+	public ResponseEntity<?> login(LoginDTO loginDTO, HttpServletResponse response) {
+		try {
+			 UserEntity result = authService.login(loginDTO);
+			Long username = result.getId();
+			Cookie cookie = new Cookie("session", username.toString());
+			cookie.setMaxAge(7*24*360);
+			cookie.setSecure(true);
+			cookie.setHttpOnly(true);
+			cookie.setPath("/");
+			response.addCookie(cookie);
+		}catch (Exception e){
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
+		return ResponseEntity.ok().build();
 	}
 	
-	@RequestMapping(method = RequestMethod.POST , value = "/api/auth/logout")
-	public String logout() {
+	@RequestMapping(method = RequestMethod.GET , value = "/api/auth/logout")
+	public String logout(@CookieValue("session") String cookie) {
 		
-		return "hello logout";
+		return "bye" + cookie;
 	}
 
 }
