95d402ec661ba77409746b6b94792c5035a0f0c1
emile bergin <berginemile098@gmail.com>
2021-06-07 08:51:14 +0200

    @dev Docker for application user

--- local-run.sh
+++ local-run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 rm -r Backend/target
+rm -r usr/target
 mvn install -f Backend/pom.xml -DskipTests
+mvn install -f user/pom.xml -DskipTests
 docker-compose up -d --force-recreate --build
