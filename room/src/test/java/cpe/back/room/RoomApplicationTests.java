2d946cde31cd565722b5cf4733d0283e0d700dfc
Aymrick Cougoulat <cougoulat.aymrick@gmail.com>
2021-06-07 08:35:44 +0200

    feat: all microservice ( dont work for now )

---
+++ room/src/test/java/cpe/back/room/RoomApplicationTests.java
@@ -0,0 +1,13 @@
+package cpe.back.room;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ user/src/test/java/cpe/back/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package cpe.back.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
